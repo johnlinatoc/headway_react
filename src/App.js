8298a557f5c1f1e790f78c108273f37104bfaddd
John Wade Linatoc <44657508+johnlinatoc@users.noreply.github.com>
2021-04-03 10:29:34 -0500

    Hook up App.js to graphQL sandbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./styles.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+  useQuery
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://87hxc.sse.codesandbox.io/",
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        allLinks {
+          url
+        }
+      }
+    `
+  })
+  .then((result) => console.log(result));
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </ApolloProvider>
   );
 }
