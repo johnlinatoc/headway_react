4c6b800f45b81f851a4e5112e062258675268ae2
John Wade Linatoc <44657508+johnlinatoc@users.noreply.github.com>
2021-04-03 13:04:08 -0500

    Create Copy Functionality

--- src/components/ResultsContainer.js
+++ src/components/ResultsContainer.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const ResultsContainer = (props) => {
+  const linkInput = useRef(null);
+
+  const copyText = (id) => {
+    const copyTextarea = document.querySelector(`#link-${id}`);
+    copyTextarea.focus();
+    copyTextarea.select();
+
+    try {
+      const successful = document.execCommand("copy");
+      var msg = successful ? "successful" : "unsuccessful";
+      alert("Copying text command was " + msg);
+    } catch (err) {
+      alert("Oops, unable to copy");
+    }
+  };
+
   const renderLinks = () =>
-    props.links.map((link) => {
-      return <p>{link.url}</p>;
+    props.links.map((link, index) => {
+      return (
+        <>
+          <textarea
+            ref={linkInput}
+            id={`link-${index}`}
+            readonly
+            disabled
+            style={{ resize: "none" }}
+          >
+            {link.url}
+          </textarea>
+          <button
+            className={"btn-secondary"}
+            onClick={(e) => {
+              copyText(index);
+            }}
+          >
+            Copy
+          </button>
+        </>
+      );
     });
 
   return <div>{renderLinks()}</div>;
